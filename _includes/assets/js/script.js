ad5b04048c17fd9b53091c444662d8fcf7d1f175
Scott Rod <djmtype@gmail.com>
2021-07-18 10:47:41 -0400

    lazy load bg

--- _includes/assets/js/script.js
+++ _includes/assets/js/script.js
@@ -20,3 +20,23 @@ var myOffcanvas = Froffcanvas({
 });
 
 myOffcanvas.init();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyBackgrounds = [].slice.call(document.querySelectorAll(".contact"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          lazyBackgroundObserver.unobserve(entry.target);
+        }
+      });
+    });
+
+    lazyBackgrounds.forEach(function(lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground);
+    });
+  }
+});
